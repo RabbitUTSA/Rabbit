daa63588c8111698eaf73ebbf30f5fa044cc1455
RabbitUTSA <42632231+RabbitUTSA@users.noreply.github.com>
2018-12-03 14:14:10 -0600

    Add files via upload

--- scatMPI.py
+++ scatMPI.py
@@ -8,9 +8,8 @@ Created on Wed Nov 28 00:52:36 2018
 from Crypto.Cipher import AES
 from Crypto import Random
 from mpi4py import MPI
+import tempfile
 import os
-import random
-import sys
 import time
 
 start = time.time()
@@ -26,37 +25,37 @@ key = b'Sixteen byte key'
 iv = Random.new().read(AES.block_size)
 cipher = AES.new(key, AES.MODE_CFB, iv)
 
-if rank == 0:
-    # get a list of the files to scatter
-    #for f in glob.glob(args.alignments):
-    work = ['shakespeare.txt', 'shakespeare2.txt','shakespeare3.txt']
-else:
-    work = None
-
 # File(s) to encrypt
-numfiles = 3 
+numfiles = 1000
 x = 1
 myfiledata = open("shakespeare.txt", "r")
 inputdata = myfiledata.read() + 'This is the end of the file'
 totalsize = os.path.getsize('shakespeare.txt')
+msg = ' '
 
-unit = comm.scatter(work, root=0)
+unit = comm.Scatter(inputdata, msg, root=0)
 
 
-
-
-for x in range(numfiles):
-    # open the file on a node
-    f = MPI.File.Open(comm, unit, mode)
-    # create a buffer for the data of size f.Get_size()
-    ba = bytearray(f.Get_size())
-    # read the contents into a byte array
-    f.Iread(ba)
-    msg = iv + cipher.encrypt(f)
-    filesize = os.path.getsize(work)
+for x in range(numfiles<1):
+   
+    msg = iv + cipher.encrypt(inputdata)
+    filesize = os.path.getsize(inputdata)
     totalsize += filesize
     x += 1
+    if x > numfiles:
+        break
+    
+# ===================================
+# End of node-related work
+# ===================================
 
+# gather results
+result = comm.Gather(inputdata, msg, root=0)
+# do something with result
+if rank == 0:
+    print (result)
+else:
+    result = None
 
 # Details on number and size of files
 print ("Number of files encrypted = ", x)
